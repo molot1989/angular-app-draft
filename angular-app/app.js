cf42d669381acb627d9ec13146535be37164a6ee
semianchuk <fix20152@gmail.com>
2016-10-06 12:52:38 +0300

    fix user bug

--- angular-app/app.js
+++ angular-app/app.js
@@ -431,7 +431,7 @@ angular.module('angularApp', ['ui.router','ngAnimate','videolist','gapi','yaru22
         $scope.videosUser = function(channelId){
             var deferred = $q.defer();
             var videoCategoryId = 22
-            var videoCategoryName = 'videoCategory'
+            var videoCategoryName = 'User'
             var data = Youtube.search({ part: 'snippet', maxResults: ($state.current.name == 'user' ? 20 : 5), pageToken: $rootScope.pageToken ? $rootScope.pageToken : '', type : 'video', channelId: channelId.id })
 
             var getData = setInterval(function(){
